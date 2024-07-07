8bdb6476c779fff35ce0c866916357aeb4bce117
asabdulkareem <as.abdulkareem@gmail.com>
2024-07-07 18:38:41 +0530

    Added Basket micro service

---
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,10 @@
+﻿global using Basket.API.Models;
+global using Carter;
+global using MediatR;
+global using Mapster;
+global using BuildingBlocks.CQRS;
+global using FluentValidation;
+global using Basket.API.Data;
+global using Marten;
+global using BuildingBlocks.Exceptions;
+global using Basket.API.Exception;
